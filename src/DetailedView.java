4b66a5763429333e42ba65c3e17c1ade2be45bbb
sw8566eq <61024900+sw8566eq@users.noreply.github.com>
2020-05-03 20:23:51 -0500

    Add files via upload

---
+++ src/DetailedView.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import CalendarObjects.*;
+
+import javax.swing.JTextArea;
+
+public class DetailedView extends JFrame {
+
+	private JPanel contentPane;
+
+	private TimeSlot timeslot;
+	private CalendarTop cal;
+	
+	public TimeSlot getTimeslot() {
+		return timeslot;
+	}
+
+	public void setTimeslot(TimeSlot timeslot) {
+		this.timeslot = timeslot;
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DetailedView frame = new DetailedView();
+				
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DetailedView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea = new JTextArea();
+		panel.add(textArea);
+	}
+
+	//need the tostring() of room, teacher, classentry and the time somewhere
+	public void fillTextArea() {
+		
+		
+		
+		
+		
+	}
+	
+}
