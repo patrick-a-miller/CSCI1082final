f5c99ea8fc7a3b2d092da118b9044f16f2fce09f
sw8566eq <61024900+sw8566eq@users.noreply.github.com>
2020-05-06 19:16:09 -0500

    Update DetailedView.java

--- src/DetailedView.java
+++ src/DetailedView.java
@@ -38,7 +38,7 @@ public class DetailedView extends JFrame {
 	 */
 	public DetailedView(TimeSlot slot) {
 		setTitle("Detailed View");
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
